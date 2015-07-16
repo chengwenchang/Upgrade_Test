e5c24825a437715eef97371d969930a48f29335d
tim <timchang@nuuo.com>
2015-07-16 16:54:42 +0800

    Fix

--- Upgrade_Test/Upgrade_Test.py
+++ Upgrade_Test/Upgrade_Test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
